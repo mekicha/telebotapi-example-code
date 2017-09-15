56242f28a7f5d5a84444df67a8d581cf486633b9
Emeka Icha <emeka.icha@gmail.com>
2017-09-15 13:27:49 +0300

    (feat) add db support: save subscribed users

---
+++ db/users.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ChatID uint 
+	FirstName string
+	LastName string
+}
+
+func Init() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "users.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&User{})
+
+	return db
+}
+
+func Registered(ChatID uint) bool {
+	db := Init()
+	defer db.Close()
+
+	var user User
+	res := db.First(&user, "chat_id = ?", ChatID)
+
+	if res.Error != nil {
+		errorMsg := res.Error.Error()
+
+		if errorMsg == "record not found" {
+			return false
+		}
+		fmt.Println(errorMsg)
+	}
+	return true
+}
+
+
+func Save(ChatID uint, FirstName, LastName string) {
+	db := Init()
+	defer db.Close()
+
+	user := User{ChatID: ChatID, FirstName: FirstName, LastName: LastName }
+
+	result := db.Create(&user)
+
+	if result.Error != nil {
+		errorMsg := result.Error.Error()
+		fmt.Println(errorMsg)
+	}
+}
