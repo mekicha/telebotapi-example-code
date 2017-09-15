22bd6a0f614a5b7f8046532f3ddc4096993ea2fe
Emeka Icha <emeka.icha@gmail.com>
2017-09-15 15:46:32 +0300

    add bot funcs

--- bot/bot.go
+++ bot/bot.go
@@ -1,34 +1,35 @@
 package bot
 
 import (
-	"net/http"
 	"log"
 	"os"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 func RunBot() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELGRAM_TOKEN"))
-
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-	bot.Debug = true 
-
+	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://www.google.com:8443/"+bot.Token, "cert.pem"))
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	updates := bot.ListenForWebhook("/" + bot.Token)
-
-
-	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.perm", "key.perm", nil)
+	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+
 	}
 }
